bb6f798e85756ced67ae98848e8762f49539a95c
Zakaria Mahmoudi <tonemail@example.com>
2025-11-23 13:02:44 +0100

    remove the array of taks

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 function TodoList(){
-    const[tasks,setTasks]=useState(['EATY','TRAINY','SLEEPY']);
+    const[tasks,setTasks]=useState([]);
     const[newtask,setNewTask]=useState("");
     function changeInput(event){
         setNewTask(event.target.value)
